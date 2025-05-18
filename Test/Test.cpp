7a6d53f5d75f4ed51f8334b8069da30a1538c957
TymianekPL <tymi@tymi.org>
2025-05-18 20:18:53 +0200

    std::formatter specialisation for json objects
    Signed-off-by: TymianekPL <tymi@tymi.org>

--- Test/Test.cpp
+++ Test/Test.cpp
@@ -5,5 +5,5 @@ int main()
 {
 	cppjson::JsonObject object{};
 	object.As<std::string>() = "Purr world!";
-	std::println("{}", object.As<std::string>());
+	std::println("{}", object);
 }
--- cppjson/include/cppjson/object.hpp
+++ cppjson/include/cppjson/object.hpp
@@ -5,6 +5,7 @@
 #include <algorithm>
 #include <string>
 #include <cstddef>
+#include <format>
 
 namespace cppjson
 {
@@ -38,5 +39,32 @@ namespace cppjson
 		T& DangerousAs() noexcept { return *std::launder(reinterpret_cast<T*>(this->_dataStorage)); }
 		template <typename T>
 		const T& DangerousAs() const noexcept { return *std::launder(reinterpret_cast<T*>(this->_dataStorage)); }
+
+		friend struct std::formatter<cppjson::JsonObject>;
+	};
+}
+
+namespace std
+{
+	template<>
+	struct formatter<cppjson::JsonObject>
+	{
+		constexpr auto parse(std::format_parse_context& context)
+		{
+			return context.begin();
+		}
+
+		auto format(const cppjson::JsonObject& object, std::format_context& context) const
+		{
+			switch (object._dataType)
+			{
+			case cppjson::JsonType::Null: return std::format_to(context.out(), "null");
+			case cppjson::JsonType::Bool: return std::format_to(context.out(), "{}", object.DangerousAs<bool>());
+			case cppjson::JsonType::Number: return std::format_to(context.out(), "{}", object.DangerousAs<double>());
+			case cppjson::JsonType::String: return std::format_to(context.out(), "{}", object.DangerousAs<std::string>());
+			}
+
+			throw std::logic_error("Unknown type");
+		}
 	};
 }
